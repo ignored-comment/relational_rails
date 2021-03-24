e21826afc870a359796ec0e0b0f020fedf1ebfe8
Aidan McGuire <>
2021-03-23 18:58:01 -0600

    Recreate garages DB, finish user story 1

--- db/migrate/20210323025342_create_garages.rb
+++ db/migrate/20210323025342_create_garages.rb
@@ -2,10 +2,10 @@ class CreateGarages < ActiveRecord::Migration[5.2]
   def change
     create_table :garages do |t|
       t.string :name
-      t.datetime :created_at
-      t.datetime :updated_at
       t.boolean :at_capacity
       t.integer :max_capacity
+
+      t.timestamps
     end
   end
 end
