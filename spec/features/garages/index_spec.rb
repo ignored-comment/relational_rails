4a7f4442db021ebaa758bcfb6856a54b0752db65
Aidan McGuire <>
2021-03-27 17:29:06 -0600

    Add tests for user story 7

--- spec/features/garages/index_spec.rb
+++ spec/features/garages/index_spec.rb
@@ -6,9 +6,20 @@ RSpec.describe 'garages index page', type: :feature do
     garage_2 = Garage.create!(name: "Aidan's Garage", at_capacity: true, max_capacity: 2)
 
     visit '/garages'
+
     expect(page).to have_content(garage_1.name)
     expect(page).to have_content(garage_1.created_at)
     expect(page).to have_content(garage_2.name)
     expect(page).to have_content(garage_2.created_at)
+  end
+
+  it "shows the number of motorcycles in the garage" do
+    garage_1 = Garage.create!(name: "Kon's Garage", at_capacity: false, max_capacity: 4)
+    garage_2 = Garage.create!(name: "Aidan's Garage", at_capacity: true, max_capacity: 2)
+  
+    visit "/garages"
+
+    expect(page).to have_content(garage_1.count_motorcycles)
+    expect(page).to have_content(garage_2.count_motorcycles)
   end
 end
--- spec/features/refrigerators/index_spec.rb
+++ spec/features/refrigerators/index_spec.rb
@@ -4,12 +4,22 @@ RSpec.describe "refrigerator index page", type: :feature do
   it "can see the name of each refrigerator record in the system" do
     refrigerator1 = Refrigerator.create!(name: "Kons Refrigerator", has_freezer: true, capacity_cubic_feet: 45)
     refrigerator2 = Refrigerator.create!(name: "Aidans Refrigerator", has_freezer: false, capacity_cubic_feet: 12)
-
+    
     visit "/refrigerators"
-
+    
     expect(page).to have_content(refrigerator1.name)
     expect(page).to have_content(refrigerator1.created_at)
     expect(page).to have_content(refrigerator2.name)
     expect(page).to have_content(refrigerator2.created_at)
+  end
+  
+  it "shows the number of foods in the refrigerator" do
+    refrigerator1 = Refrigerator.create!(name: "Kons Refrigerator", has_freezer: true, capacity_cubic_feet: 45)
+    refrigerator2 = Refrigerator.create!(name: "Aidans Refrigerator", has_freezer: false, capacity_cubic_feet: 12)
+    
+    visit "/refrigerators"
+
+    expect(page).to have_content(refrigerator1.count_foods)
+    expect(page).to have_content(refrigerator2.count_foods)
   end
 end 
